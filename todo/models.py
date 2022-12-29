9f09f0acc7973eaf22f9bb692c9125a1ae483a10
Farah M <farah.maria.rahman@gmail.com>
2022-12-29 20:28:41 +0000

    create Job class in models.py

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Job(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True,
+                             blank=True)
+    header = models.CharField(max_length=210, null=True)
+    info = models.TextField(null=True, blank=True)
+    done = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.header
+
+    class Meta:
+        ordering = ['done']
