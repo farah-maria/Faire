13cd7cc04b61e3ed9261e790b986d554376b4046
Farah M <farah.maria.rahman@gmail.com>
2022-12-30 00:57:54 +0000

    create skeleton job_list.html file

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import JobList
 
 urlpatterns = [
-    path('', views.home)
+    path('', JobList.as_view(), name="Job")
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+
+from .models import Job
 
 
-def home(request):
-    return HttpResponse("Working")
+class JobList(ListView):
+    model = Job
