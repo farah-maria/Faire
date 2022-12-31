c568750a0bebb709acf2d7c95db1262b0a4edd0c
Farah M <farah.maria.rahman@gmail.com>
2022-12-31 21:23:26 +0000

    create job form basic functionality for creating a job via the frontend

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import JobList, JobDetail
+from .views import JobList, JobDetail, JobCreate
 
 urlpatterns = [
     path('', JobList.as_view(), name="Job"),
+    path('job-create/', JobCreate.as_view(), name="job-create"),
     path('job/<int:pk>/', JobDetail.as_view(), name="jobs-detail")
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Job
 
 
@@ -14,3 +16,10 @@ class JobDetail(DetailView):
     model = Job
     context_object_name = 'job'
     template_name = 'todo/job_detail.html'
+    
+
+class JobCreate(CreateView):
+    model = Job
+    fields = "__all__"
+    
+    
