801cdcc4a7b3b662c105757cb936ce2dd546e660
Farah M <farah.maria.rahman@gmail.com>
2023-01-01 20:50:03 +0000

    add job update url and class in views as well as button on job list html page

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import JobList, JobDetail, JobCreate
+from .views import JobList, JobDetail, JobCreate, JobUpdate
 
 urlpatterns = [
     path('', JobList.as_view(), name="job"),
     path('job-create/', JobCreate.as_view(), name="job-create"),
-    path('job/<int:pk>/', JobDetail.as_view(), name="jobs-detail")
+    path('job/<int:pk>/', JobDetail.as_view(), name="jobs-detail"),
+    path('job-update/<int:pk>/', JobUpdate.as_view(), name="jobs-update")
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Job
 
@@ -19,6 +19,12 @@ class JobDetail(DetailView):
     
 
 class JobCreate(CreateView):
+    model = Job
+    fields = '__all__'
+    success_url = reverse_lazy('job')
+
+
+class JobUpdate(UpdateView):
     model = Job
     fields = '__all__'
     success_url = reverse_lazy('job')
