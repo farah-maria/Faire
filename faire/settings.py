c58e8763536f311e6536f8745578a87997893b69
Farah M <farah.maria.rahman@gmail.com>
2023-01-10 17:37:23 +0000

    add static file at root level, where it should have been

--- faire/settings.py
+++ faire/settings.py
@@ -137,9 +137,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
